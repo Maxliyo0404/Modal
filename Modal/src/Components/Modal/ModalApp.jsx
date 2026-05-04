65a05538442cb646b95b1ffa0135a7ef4ce793d7
Mahliyo <gasandigital13@gmail.com>
2026-05-04 21:40:52 +0500

    Modal reactda kod

--- Modal/src/Components/Modal/ModalApp.jsx
+++ Modal/src/Components/Modal/ModalApp.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "/.Modal.css";
 function ModalApp (){
-    const [modal, setModL]
+    const [modal, setModL] = useState(false);
     return (
         <>
-        
+         <h1 className="title">Mo</h1>
         </>
     )
 }
