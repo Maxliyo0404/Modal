7ae18d121693b0543f0bf75bf0b6e0d81075fb01
Mahliyo <gasandigital13@gmail.com>
2026-05-04 22:08:16 +0500

    modal false

--- Modal/src/Components/Modal/ModalApp.jsx
+++ Modal/src/Components/Modal/ModalApp.jsx
@@ -7,16 +7,14 @@ function ModalApp (){
         <div className="container">
          <h1 className="title">Modal  Verbs</h1>
          <button className="btn" onClick={()=> setModL(true)}>Open Modal</button>
-         {
-            modal && (
+         { modal && (
                 <div className="box">
                     <h2 className="box-title">Modal Verbs List</h2>
                     <button className="box-btn" onClick={()=> setModL(false)}>Close</button>
                 </div>
-            )
-         }
+            )}
          </div>
         </>
-    )
+    );
 }
 export default ModalApp;
