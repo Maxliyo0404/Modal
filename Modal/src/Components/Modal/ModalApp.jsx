89441b0ac9e7199d27ae5421061669b5b3e4f26d
Mahliyo <gasandigital13@gmail.com>
2026-05-04 21:45:09 +0500

    Modal usestate

--- Modal/src/Components/Modal/ModalApp.jsx
+++ Modal/src/Components/Modal/ModalApp.jsx
@@ -4,7 +4,10 @@ function ModalApp (){
     const [modal, setModL] = useState(false);
     return (
         <>
+        <div className="container">
          <h1 className="title">Modal  Verbs</h1>
+         <button className="btn" onClick={()=> setModL(true)}></button>
+         </div>
         </>
     )
 }
