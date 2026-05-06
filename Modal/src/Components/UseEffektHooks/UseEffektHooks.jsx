35aeadb6af38482562b8d7e1d4f071fac42a2802
Mahliyo <gasandigital13@gmail.com>
2026-05-06 15:02:19 +0500

    UseEffektHooks css cards title

--- Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
+++ Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
@@ -20,7 +20,8 @@ const Card = () => {
                     <div className="card" key={el.id}>
                         <img className="rasm" src={el.avatar} alt={el.name} />
                         <h1 className="title">{el.name}</h1>
-                        <a href={el.createdAt}></a>
+                      <p class="text">${el.username }</p>
+                     <a class="email" href="#">${el.email}</a>
                     </div>
                 ))
            
