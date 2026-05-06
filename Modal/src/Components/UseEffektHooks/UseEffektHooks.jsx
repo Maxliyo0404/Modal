c76d22e65e0da7804d47a35d51e63a893aff8876
Mahliyo <gasandigital13@gmail.com>
2026-05-06 11:21:46 +0500

    UseEffektHooks fetch

--- Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
+++ Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
@@ -2,5 +2,8 @@ import { useEffect, useState } from "react";
 import "./UseEffektHooks.css";
  const Card = () => {
     const  [data, setData] = useState([]);
-    useEffect {( )}
+    useEffect {() =>{
+        fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
+        
+    }}
  }
