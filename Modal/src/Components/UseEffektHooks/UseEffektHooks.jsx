01068400f207a14402ab881ae1e79574709885ee
Mahliyo <gasandigital13@gmail.com>
2026-05-06 11:25:44 +0500

    UseEffektHooks fetch

--- Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
+++ Modal/src/Components/UseEffektHooks/UseEffektHooks.jsx
@@ -4,6 +4,9 @@ import "./UseEffektHooks.css";
     const  [data, setData] = useState([]);
     useEffect {() =>{
         fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
-        
+        .then(data => data.json())
+        .then(data =>{
+            setData(data);
+        })
     }}
- }
+ },[]
